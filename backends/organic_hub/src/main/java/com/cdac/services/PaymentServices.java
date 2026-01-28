eff250cec99c9a10fe4e0cc4b740f9528ce6a35a
RohitPawar2003 <165992291+RohitPawar2003@users.noreply.github.com>
2026-01-28 20:06:12 +0530

    Delete backends/organic_hub directory

--- backends/organic_hub/src/main/java/com/cdac/services/PaymentServices.java
+++
@@ -1,9 +0,0 @@
-package com.cdac.services;
-
-import com.cdac.modelmvc.Payment;
-
-public interface PaymentServices {
-	Payment makePayments(Payment payment);
-
-	Payment getpaymentByOrderId(Long orderId);
-}
